5762e48f15a767e668a49132894db14782a1f976
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-04-22 02:02:46 -0700

    Main page & google maps
    Google maps autocomplete and location pinning complete

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
-import { store } from './store';
-import HomeScreen from './screens/HomeScreen';
-
-
+import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  //equivalent to router
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,61 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
-import React from 'react'
-import tw from 'tailwind-react-native-classnames'
-import NavOptions from '../components/NavOptions'
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
-    return (
-        <SafeAreaView style={`bg-white h-full`}>
-            <View style={tw`p-5`}>
-                <Image
-                    style={{ width: 100, height: 100, resizeMode: 'contain' }}
-                    source={{
-                        uri: "https://links.papareact.com/gzs"
-                    }}
-                />
+  const dispatch = useDispatch();
 
-                <NavOptions />
-            </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
 
-export default HomeScreen
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI="GooglePlacesSearch"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          debounce={400}
+          placeholder="Where are we headed?"
+        />
 
-const styles = StyleSheet.create({})
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
