23e37fa6dc1d4a368d839b7e4a4f673810dfe0c8
CHEN TZEN KOK <chentzen02@g.ucla.edu>
2023-04-22 04:10:33 -0700

    create dropdown
    create search bar filter system
    
    filter male and female

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SearchBar from '../components/SearchBar'
+
+const ProfileScreen = ({navigation}) => {
+  return (
+    <SearchBar/>
+  )
+}
+
+export default ProfileScreen
