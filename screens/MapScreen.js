11da444bc57a80178c42be9f1499eedea89663f6
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-04-22 02:58:53 -0700

    Have the map fill up the whole screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,14 +3,14 @@ import React from "react";
 import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import MapView from "react-native-maps";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigateCard from "../components/NavigateCard";
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <View style={tw`h-1/2`}>
-        <Map />
-      </View>
-      <View style={tw`h-1/2`}></View>
+    <View style={tw`flex-1`}>
+      <Map />
     </View>
   );
 };
