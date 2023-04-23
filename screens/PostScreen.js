098396acd2b08d1abcd2b9d4d7657d91e7586d2e
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-04-22 23:10:11 -0700

    Posting functionality done

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -11,26 +11,55 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const PostScreen = () => {
-  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
   const [locations, setLocations] = useState([]);
 
   const handleLocationSelect = (data, details) => {
-    setSelectedLocation(details.geometry.location);
-    setLocations([...locations, details.geometry.location]);
+    const name = details.name;
+    setLocations([...locations, name]);
   };
 
-  const renderLocation = ({ item }) => {
-    return (
-      <View style={styles.locationItem}>
-        <Text style={styles.locationText}>
-          {item.lat}, {item.lng}
-        </Text>
-      </View>
-    );
+  const handleSubmit = () => {
+    // Here you can submit the title, body, and location array to your API or database
+    console.log("Title:", title);
+    console.log("Body:", body);
+    console.log("Locations:", locations);
+    setTitle("");
+    setBody("");
+    setLocations([]);
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.label}>Title:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter title"
+          value={title}
+          onChangeText={setTitle}
+        />
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.label}>Body:</Text>
+        <TextInput
+          style={[styles.input, styles.bodyInput]}
+          placeholder="Enter body"
+          value={body}
+          onChangeText={setBody}
+          multiline={true}
+          numberOfLines={4}
+        />
+      </View>
+      {locations.length > 0 && (
+        <View style={styles.locationsContainer}>
+          <Text style={styles.label}>Location Saved:</Text>
+          {locations.map((location, index) => (
+            <Text key={index}>{location}</Text>
+          ))}
+        </View>
+      )}
       <GooglePlacesAutocomplete
         placeholder="Search Location"
         onPress={handleLocationSelect}
@@ -44,23 +73,7 @@ const PostScreen = () => {
           textInput: styles.input,
         }}
       />
-      {selectedLocation && (
-        <View style={styles.selectedLocationContainer}>
-          <Text style={styles.selectedLocationText}>
-            Selected Location: {selectedLocation.lat}, {selectedLocation.lng}
-          </Text>
-        </View>
-      )}
-      {locations.length > 0 && (
-        <View style={styles.locationsContainer}>
-          <Text style={styles.locationsTitle}>Locations:</Text>
-          <FlatList
-            data={locations}
-            renderItem={renderLocation}
-            keyExtractor={(item, index) => index.toString()}
-          />
-        </View>
-      )}
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
 };
@@ -70,36 +83,29 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
-  autocompleteContainer: {
-    borderWidth: 0,
+  titleContainer: {
+    marginBottom: 20,
+  },
+  bodyContainer: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 5,
   },
   input: {
-    height: 40,
+    fontSize: 16,
     borderWidth: 1,
-    borderRadius: 10,
+    borderColor: "#ccc",
+    borderRadius: 4,
     padding: 10,
   },
-  selectedLocationContainer: {
-    marginTop: 20,
-  },
-  selectedLocationText: {
-    fontSize: 16,
+  bodyInput: {
+    height: 120,
+    textAlignVertical: "top",
   },
   locationsContainer: {
-    marginTop: 20,
-  },
-  locationsTitle: {
-    fontSize: 16,
-    fontWeight: "bold",
     marginBottom: 10,
-  },
-  locationItem: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-    paddingVertical: 10,
-  },
-  locationText: {
-    fontSize: 14,
   },
 });
 
