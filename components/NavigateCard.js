a3fac33c6e6b527245763b00285144bcaa6b4aa8
Joonwon (Eric) Lee <joonwontech@gmail.com>
2023-04-22 18:27:22 -0700

    Finished Tabs
    Have different cards for each tab

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,16 +1,85 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import tw from "tailwind-react-native-classnames";
 
-
 const NavigateCard = () => {
+  const [selectedTab, setSelectedTab] = useState("nearYou");
 
-  
+  const handleTabPress = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  const renderNearYouCards = () => {
+    return (
+      <ScrollView horizontal={true}>
+        <View style={{ width: 200, backgroundColor: "#f2f2f2", margin: 10 }}>
+          <Text style={{ padding: 10 }}>Card 1</Text>
+        </View>
+        <View style={{ width: 200, backgroundColor: "#f2f2f2", margin: 10 }}>
+          <Text style={{ padding: 10 }}>Card 2</Text>
+        </View>
+        <View style={{ width: 200, backgroundColor: "#f2f2f2", margin: 10 }}>
+          <Text style={{ padding: 10 }}>Card 3</Text>
+        </View>
+      </ScrollView>
+    );
+  };
+
+  const renderRecentlyAddedCards = () => {
+    // implementation for recently added cards
+  };
+
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center py-5 text-xl`}>Recommended Plans!</Text>
-      <View style={tw`border-t border-gray-200 flex-shrink`}>
+      <View
+        style={tw`flex-row justify-between items-center p-4 border-b border-gray-200`}
+      >
+        <TouchableOpacity
+          style={[
+            tw`flex-1`,
+            selectedTab === "nearYou" && tw`border-b-2 border-blue-500`,
+          ]}
+          onPress={() => handleTabPress("nearYou")}
+        >
+          <Text
+            style={[
+              tw`text-center font-bold`,
+              selectedTab === "nearYou" && tw`text-blue-500`,
+              { fontSize: 14 },
+            ]}
+          >
+            Near You
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            tw`flex-1`,
+            selectedTab === "recentlyAdded" && tw`border-b-2 border-blue-500`,
+          ]}
+          onPress={() => handleTabPress("recentlyAdded")}
+        >
+          <Text
+            style={[
+              tw`text-center font-bold`,
+              selectedTab === "recentlyAdded" && tw`text-blue-500`,
+              { fontSize: 14 },
+            ]}
+          >
+            Recently Added
+          </Text>
+        </TouchableOpacity>
       </View>
+      {selectedTab === "nearYou"
+        ? renderNearYouCards()
+        : renderRecentlyAddedCards()}
+      <View style={tw`border-t border-gray-200 flex-shrink`} />
     </SafeAreaView>
   );
 };
